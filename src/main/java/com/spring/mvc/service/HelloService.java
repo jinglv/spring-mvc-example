811354cb4adae2cdafd529c2ffe37054e3af4069
jinglv <lvjing0705@163.com>
2020-04-06 19:51:36 +0800

    first commit

---
+++ src/main/java/com/spring/mvc/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.spring.mvc.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author jinglv
+ * @date 2020-04-06 19:02
+ */
+@Service
+public class HelloService {
+    public String sayHello() {
+        return "Hello, Spring MVC 开发框架！";
+    }
+}
